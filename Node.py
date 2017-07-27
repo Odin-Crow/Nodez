25748064f67ab08b9a50613f0cd5ff465cb34d36
Odin-Crow <the.old.odin.crow@gmail.com>
2017-07-27 10:21:37 -0500

    Trying to get flat file for nodes right
    Full of errors

--- Node.py
+++ Node.py
@@ -2,7 +2,6 @@
 class Node:
 
 
-
     def __init__(self, n="Node", d="0/0/0", det={}):
         self.Name = n
         self.Date=d
@@ -11,6 +10,58 @@ class Node:
 
     def addConnect(self, conn):
         self.Connections.append(conn)
+
+
+    def saveToFile(self, node):
+
+        file = open('nodes.csv','r')
+        found = False
+        #Find line
+        lines = file.readlines()
+
+        RelLineNumber = 0
+        for line in lines:
+
+            spl = line.split(',')
+
+            if spl[0] == node.Name:
+                found = True
+
+            else:
+                RelLineNumber+=1
+        file.close()
+        #If found, append
+        if found == True:
+            file = open('nodes.csv','r+')
+            print(RelLineNumber)
+            arrLines = []
+
+            for line in file.readlines():
+
+                arrLines.append(line)
+            print(len(arrLines))
+            arrLines[RelLineNumber] = arrLines[RelLineNumber] + node.Name
+            for line in arrLines:
+                file.write(line)
+            file.close()
+
+
+        #If not found, new line
+        if found == False:
+            file = open('nodes.csv','r+')
+            print('[DEBUG] New Node Line')
+            connections = []
+            connections.append(node)
+            for n in node.Connections:
+                connections.append(n)
+
+            for n in connections:
+                file.write(n.Name+',')
+
+            file.write('\n')
+
+        #Close
+        file.close()
 
 class Connection:
 
@@ -23,5 +74,5 @@ class Connection:
         self.Node1 = Node1
         self.Node2 = Node2
 
-        Node1.addConnect(self)
-        Node2.addConnect(self)
+        Node1.addConnect(self.Node2)
+        Node2.addConnect(self.Node1)
--- test.py
+++ test.py
@@ -10,12 +10,20 @@ def main():
     connect12 = Connection('conn1_Name','0/0/3',{'This_Conn':'This_Value'})
     connect13 = Connection('conn_1to3')
     connect14 = Connection('conn_1to4')
+    connect42 = Connection('conn_4to2')
 
     connect12.Connect(node1, node2)
     connect13.Connect(node1, node3)
     connect14.Connect(node1, node4)
+    connect42.Connect(node4, node2)
+
+    node1.saveToFile(node1)
+    node2.saveToFile(node2)
+    node3.saveToFile(node3)
+    node4.saveToFile(node4)
     print(connect12.Node1.Name+' is the name of Node 1\n'+connect12.Node2.Name+' is the name of Node 2\n')
     for conn in node1.Connections:
-        print(conn.Node1.Name + ' is connected to '+ conn.Node2.Name)
+        print(conn.Name + ' is connected to '+ conn.Name)
+
 if __name__ == '__main__':
     main()
